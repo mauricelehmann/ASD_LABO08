c10269a22c6a37670aa435ec920eee19fec9c447
Maurice <m.lehmann94@gmail.com>
2019-05-15 15:32:55 +0200

    selectionSort forwardList : codecheck ok

--- selectionSort - forwardList/selectionSort.h
+++ selectionSort - forwardList/selectionSort.h
@@ -5,10 +5,24 @@
 
 template <class T>
 void selectionSort(std::forward_list<T>& L) {
-  using namespace std;
 
 //  Complétez cette fonction
-
+	if(!L.empty()){
+		auto j = L.before_begin();
+		while(next(next(j)) != L.end())
+		{
+			auto min = next(j);
+			auto i = j;
+			while(next(i) != L.end()){
+				if(*next(i) < *next(min))
+					min = i;
+				++i;
+			}
+			L.splice_after(j,L,min);
+			++j;
+			std::cout << L << "\n";
+		}
+	}
 }
 
 #endif
