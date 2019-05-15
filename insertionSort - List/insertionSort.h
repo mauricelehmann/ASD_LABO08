f47b9542b12dfc2df0c6dcec3dece8f50cc597a4
Nicolas Hungerbühler <31136697+Grimlix@users.noreply.github.com>
2019-05-15 15:22:20 +0200

    passe le codecheck

--- insertionSort - List/insertionSort.h
+++ insertionSort - List/insertionSort.h
@@ -1,15 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-#ifndef insertionsort_h
-#define insertionsort_h
+/* 
+ * File:   insertionSort.h
+ * Author: nichu
+ *
+ * Created on 15. mai 2019, 14:23
+ */
+
+#ifndef INSERTIONSORT_H
+#define INSERTIONSORT_H
 
 #include "listUtils.h"
 
 template <class T>
 void insertionSort(std::list<T>& L) {
-  using namespace std;
-
-//  Complétez le code de cette fonction
-
+  if(L.empty()){
+     return;
+  }
+  auto k = next(L.begin());
+  while(k != L.end()){
+     auto i = k;
+     while(i != L.begin() && *k < *(prev(i))){
+        i = prev(i);
+     }
+     auto t = next(k);
+     L.splice(i,L,k);
+     k = t;
+     std::cout << L << std::endl;
+  }
 }
 
-#endif // insertionsort_h
+
+
+#endif /* INSERTIONSORT_H */
+
