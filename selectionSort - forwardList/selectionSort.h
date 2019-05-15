a167413203f2b9f7a6c990dd13386fb03663e7d8
Maurice <m.lehmann94@gmail.com>
2019-05-15 15:34:21 +0200

    beau ça

--- selectionSort - forwardList/selectionSort.h
+++ selectionSort - forwardList/selectionSort.h
@@ -7,21 +7,22 @@ template <class T>
 void selectionSort(std::forward_list<T>& L) {
 
 //  Complétez cette fonction
-	if(!L.empty()){
-		auto j = L.before_begin();
-		while(next(next(j)) != L.end())
-		{
-			auto min = next(j);
-			auto i = j;
-			while(next(i) != L.end()){
-				if(*next(i) < *next(min))
-					min = i;
-				++i;
-			}
-			L.splice_after(j,L,min);
-			++j;
-			std::cout << L << "\n";
+	if(L.empty()){
+		return;
+	}
+	auto j = L.before_begin();
+	while(next(next(j)) != L.end())
+	{
+		auto min = next(j);
+		auto i = j;
+		while(next(i) != L.end()){
+			if(*next(i) < *next(min))
+				min = i;
+			++i;
 		}
+		L.splice_after(j,L,min);
+		++j;
+		std::cout << L << "\n";
 	}
 }
 
